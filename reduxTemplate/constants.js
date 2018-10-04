4f98b58392be3561db0eb268cac8fa6a66801c28
Maximilien Garenne <maximilien@whatson-web.com>
2018-10-04 09:49:55 +0200

    [integration] clean

--- reduxTemplate/constants.js
+++ reduxTemplate/constants.js
@@ -1,13 +1,14 @@
 import { createTypes } from 'redux-friends';
-
+// -------------reducer entry key ---------------- //
 export const MY_RK = 'someKey';
-
+// ---------------- actions Types  ----------------- //
 export const SET_AAA = 'SET_AAA';
 export const SET_BBB = 'SET_BBB';
-
+// ------------ reducer keys -------------------- //
 export const AAA = 'someAAA';
 export const BBB = 'someBBB';
+// --------- types Array -------------- //
 
 export const MyTypesArray = [SET_AAA, SET_BBB];
-
+// -------------- types ---------------- //
 export const TYPES = createTypes(MY_RK, MyTypesArray);
